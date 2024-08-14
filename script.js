37f1d743e48800b9b29225871c6034ee9eae193c
pskhobragade45 <pskhobragade45@gmail.com>
2024-08-15 00:25:23 +0530

    Add all Signature App

---
+++ script.js
@@ -0,0 +1,29 @@
+// const colorPicker = document.getElementById('colorPicker');
+// const canvasColor = document.getElementById('canvasColor');
+// const canvas = document.getElementById('canvas');
+// const clearButton = document.getElementById('clearButton');
+// const saveButton = document.getElementById('saveButton');
+// const fontSize = document.getElementById('fontSize');
+// const retrieveButton = document.getElementById('retrieveButton');
+// const ctx = canvas.getContext('2d');
+
+// colorPicker.addEventListener('change',(e) =>{
+//     ctx.strokeStyle = e.target.value;
+//     ctx.fillStyle = e.target.value;
+// });
+
+// canvas.addEventListener('mousedown',(e) =>{
+//     isDrawing = true;
+//     lastX = Event.offsetX;
+//     lastY = Event.offsetY;
+// })
+
+// canvas.addEventListener('mousemove', (e) =>{
+//     if(isDrawing){
+//         ctx.beingPAth();
+//         ctx.moveTo(lastX,lastY);
+//         ctx.lineTo(Event.offsetX,Event.offsetY);
+//         ctx.stroke();
+//     }
+// })
+
